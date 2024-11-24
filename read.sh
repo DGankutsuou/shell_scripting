e9cf607c4eabab60caf9d89e6d68e48fd5f976c8
DGankutsuou <DGankutsuou@github.com>
2024-11-24 16:28:38 +0100

    LINUX

---
+++ read.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+NIP=`hostname -I`
+
+echo Thy name:
+read NAME
+echo #will print space
+echo welcom $NAME into the Blasphomy
+echo connect through $NIP
+echo
---
+++ variables.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+NAME=Nameless
+GREET='Welcome to the Blasphemy'
+# no space between var and '='
+# if Thou have more than word put in '' or ""
+echo $NAME
+echo $GREET
